9c5207d4774dbb63f4032d195f05313223ce2c59
Salim Ghanem <saleemghanem@gmail.com>
2024-06-10 16:54:02 +0200

    use client quill

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,9 @@
 "use client";
 import { use } from "react";
-
-// import Resume from "./resume/page";
-
+import Resume from "./resume/page";
 import { useAppContext } from "@/context/AppContext";
 import NavBar from "./components/NavBar";
-import dynamic from "next/dynamic";
-const Resume = dynamic(() => import("./resume/page"), {
-  ssr: false,
-});
+
 export default function Home() {
   const { state } = useAppContext();
   console.log("state", state);
