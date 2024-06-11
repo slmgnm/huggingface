b2de766aee6ff74f09123d0eac897e91ea60d267
Salim Ghanem <saleemghanem@gmail.com>
2024-06-11 08:52:42 +0200

    fix reactquill

--- app/components/RichText.tsx
+++ app/components/RichText.tsx
@@ -1,7 +1,7 @@
 "use client";
 import dynamic from "next/dynamic";
-
-const ReactQuill = dynamic(import("react-quill"), { ssr: false });
+import ReactQuill from "react-quill";
+// const ReactQuill = dynamic(import("react-quill"), { ssr: false });
 
 import "react-quill/dist/quill.snow.css";
 
